53b4276e5b827cb24c8b4d07c25218f458344645
Adam Hintz <adam.0.dev@gmail.com>
2008-11-30 00:01:11 -0800

    Physics doesn't work now... it's frustrating the heck out of me...

--- Resources/Scripts/Modes/Demo.lua
+++ Resources/Scripts/Modes/Demo.lua
@@ -11,6 +11,8 @@
 -- Use other Heavy Cruisers (possibly built on planets) to destroy Carrier, using attack command.
 
 import('Physics')
+ship = PhysicsObject(1000.0, { 0, 0 }, { 0, 0 }, 0) -- a one thousand tonne ship
+
 -- import('Bullet4Demo')
 
 ships = {}
@@ -25,13 +27,17 @@ hCruiserRotation = 0
 hCruiserSize = {}
 hCruiserSize[1], hCruiserSize[2] = graphics.sprite_dimensions("Ishiman/HeavyCruiser")
 --velocity = { increment = { x = 0, y = 0 }, real = { speed = 0, x = 0, y = 0, rot = 0 }, increase = 0.1, decrease = -0.2, max = 5 }
-ship = PhysicsObject(1000.0) -- a one thousand tonne ship
 twothirdspi = 2.0 / 3.0 * math.pi
 fivesqrt3 = (5 * math.sqrt(3))
+local sigma = 0
 drawshot = false
-shotrot = 0
 shotfired = 0
 shot = { x = 0, y = 0, move = 0 }
+local frame = 0
+local ddt = 0
+arrowLength = 30
+local bulletFired = false
+local bulletRotation = 0
 
 keysDown = { accelerate = false, reverse = false, left = false, right = false }
 
@@ -48,6 +54,9 @@ end
 function hypot(x, y)
     return math.sqrt(x*x + y*y)
 end
+
+local arrowDist = hypot(10, (100 - fivesqrt3))
+local arrowAlpha = math.atan2(10, 100 - fivesqrt3)
 
 function update ()
     local newTime = mode_manager.time()
@@ -98,6 +107,10 @@ end
 function render ()
     graphics.begin_frame()
 	
+    local newTime = mode_manager.time()
+    local dt = newTime - lastTime
+    lastTime = newTime
+	
 --	heavy_cruiser_rotation = ship:angle()
 	local shipLocation = ship:location()
 	graphics.set_camera(shipLocation.x - (camera.width / 2.0), shipLocation.y - (camera.height / 2.0), shipLocation.x + (camera.width / 2.0), shipLocation.y + (camera.width / 2.0))
@@ -105,46 +118,37 @@ function render ()
 		graphics.draw_sprite("Gaitori/Carrier", carrierLocation[1], carrierLocation[2], carrierSize[1], carrierSize[2], carrierRotation)
     else
 		if carrierExploded == false then -- I need a wait() or timer or something to delay the flash of the explosion
-			sound.play("New/ExplosionCombo")
+			if frame == 0 then
+				sound.play("New/ExplosionCombo")
+			end
 			local explosion = {}
 			explosion[1], explosion[2] = graphics.sprite_dimensions("Explosions/BestExplosion")
-			graphics.draw_sprite("Explosions/BestExplosion", carrierLocation[1], carrierLocation[2], explosion[1], explosion[2], 0)
-			graphics.draw_sprite("Explosions/BestExplosion", carrierLocation[1], carrierLocation[2], explosion[1], explosion[2], 1 / 6 * math.pi)
-			graphics.draw_sprite("Explosions/BestExplosion", carrierLocation[1], carrierLocation[2], explosion[1], explosion[2], 1 / 3 * math.pi)
-			graphics.draw_sprite("Explosions/BestExplosion", carrierLocation[1], carrierLocation[2], explosion[1], explosion[2], 1 / 2 * math.pi)
-			graphics.draw_sprite("Explosions/BestExplosion", carrierLocation[1], carrierLocation[2], explosion[1], explosion[2], 2 / 3 * math.pi)
-			graphics.draw_sprite("Explosions/BestExplosion", carrierLocation[1], carrierLocation[2], explosion[1], explosion[2], 5 / 6 * math.pi)
-			graphics.draw_sprite("Explosions/BestExplosion", carrierLocation[1], carrierLocation[2], explosion[1], explosion[2], math.pi)
-			graphics.draw_sprite("Explosions/BestExplosion", carrierLocation[1], carrierLocation[2], explosion[1], explosion[2], 7 / 6 * math.pi)
-			graphics.draw_sprite("Explosions/BestExplosion", carrierLocation[1], carrierLocation[2], explosion[1], explosion[2], 4 / 3 * math.pi)
-			graphics.draw_sprite("Explosions/BestExplosion", carrierLocation[1], carrierLocation[2], explosion[1], explosion[2], 3 / 2 * math.pi)
-			graphics.draw_sprite("Explosions/BestExplosion", carrierLocation[1], carrierLocation[2], explosion[1], explosion[2], 5 / 3 * math.pi)
-			graphics.draw_sprite("Explosions/BestExplosion", carrierLocation[1], carrierLocation[2], explosion[1], explosion[2], 11 / 6 * math.pi)
-			carrierExploded = true
+			graphics.draw_sprite("Explosions/BestExplosion", carrierLocation[1], carrierLocation[2], explosion[1], explosion[2], frame / 6 * math.pi)
+			if frame == 12 then
+				carrierExploded = true
+			else
+				frame = frame + 1
+			end
 		end
 	end
 	graphics.draw_sprite("Ishiman/HeavyCruiser", shipLocation.x, shipLocation.y, hCruiserSize[1], hCruiserSize[2], ship:angle())
-
---	these three lines cause errors???
---	graphics.draw_line(100 + fivesqrt3, 0, 100 - fivesqrt3, -10, 3, ship:angle())
---	graphics.draw_line(100 - fivesqrt3, -10, 100 - fivesqrt3, 10, 3, ship:angle())
---	graphics.draw_line(100 - fivesqrt3, 10, 100 + fivesqrt3, 0, 3, ship:angle())
 	
-	--[[ship.shift.x = ship.x + math.cos(hCruiserRotation) * 100
-	ship.shift.y = ship.y + math.sin(hCruiserRotation) * 100
-	ship.shift.a.x = ship.x + math.cos(hCruiserRotation) * 100 + math.cos(hCruiserRotation) * 6
-	ship.shift.a.y = ship.y + math.sin(hCruiserRotation) * 100 + math.sin(hCruiserRotation) * 6 
-	ship.shift.b.x = ship.x + math.cos(hCruiserRotation) * 100 + math.cos(hCruiserRotation + twothirdspi) * 6
-	ship.shift.b.y = ship.y + math.sin(hCruiserRotation) * 100 + math.sin(hCruiserRotation + twothirdspi) * 6
-	ship.shift.c.x = ship.x + math.cos(hCruiserRotation) * 100 + math.cos(hCruiserRotation - twothirdspi) * 6
-	ship.shift.c.y = ship.y + math.sin(hCruiserRotation) * 100 + math.sin(hCruiserRotation - twothirdspi) * 6
+	if firebullet == true then
+		fire_bullet(shipLocation.x, shipLocation.y, ship:angle())
+		firebullet = false
+		bulletFired = true
+	end
+	if bulletFired == true then
+		moving_bullet()
+	end
+	
+	
 	if drawshot == true then
-		-- shotrot = hCruiserRotation
-		-- How do I get this equal to the ship's rotation??
-		shot.x = ship.x
-		shot.y = ship.y
+		bulletRotation = ship:angle()
+		shot.x = shipLocation.x
+		shot.y = shipLocation.y
 		shot.move = 19
-		graphics.draw_line(shot.x + math.cos(shotrot) * 17, shot.y + math.sin(shotrot) * 17, shot.x + math.cos(shotrot) * 52, shot.y + math.sin(shotrot) * 52, 2)
+		graphics.draw_line(shot.x + math.cos(bulletRotation) * 17, shot.y + math.sin(bulletRotation) * 17, shot.x + math.cos(bulletRotation) * 52, shot.y + math.sin(bulletRotation) * 52, 2)
 		drawshot = false
 		shotfired = true
 	end
@@ -153,27 +157,22 @@ function render ()
 		if shot.move >= 120 then
 			shotfired = false
 		end
-		graphics.draw_line(shot.x + math.cos(shotrot) * shot.move, shot.y + math.sin(shotrot) * shot.move, shot.x + math.cos(shotrot) * (40 + shot.move), shot.y + math.sin(shotrot) * (40 + shot.move), 2)
+		graphics.draw_line(shot.x + math.cos(bulletRotation) * shot.move, shot.y + math.sin(bulletRotation) * shot.move, shot.x + math.cos(bulletRotation) * (40 + shot.move), shot.y + math.sin(bulletRotation) * (40 + shot.move), 2)
 	end
-	if firebullet == true then
-		fire_bullet(ship.x, ship.y, hCruiserRotation)
-		firebullet = false
-		bulletfired = true
-	end
-	if bulletfired == true then
-		
-	end
-	graphics.draw_line(ship.shift.a.x, ship.shift.a.y, ship.shift.b.x, ship.shift.b.y, 2)
-	graphics.draw_line(ship.shift.b.x, ship.shift.b.y, ship.shift.c.x, ship.shift.c.y, 2)
-	graphics.draw_line(ship.shift.c.x, ship.shift.c.y, ship.shift.a.x, ship.shift.a.y, 2)
-	--]]
-	-- this is hacky code, it should have a call to graphics.set_camera here
+	
+	
+	graphics.set_camera(-camera.width / 2.0, -camera.height / 2.0, camera.width / 2.0, camera.width / 2.0)
+	
+	graphics.draw_line(math.cos(arrowAlpha + ship:angle()) * arrowDist, math.sin(arrowAlpha + ship:angle()) * arrowDist, math.cos(ship:angle() - arrowAlpha) * arrowDist, math.sin(ship:angle() - arrowAlpha) * arrowDist, 2)
+	graphics.draw_line(math.cos(ship:angle() - arrowAlpha) * arrowDist, math.sin(ship:angle() - arrowAlpha) * arrowDist, math.cos(ship:angle()) * (100 + fivesqrt3), math.sin(ship:angle()) * (100 + fivesqrt3), 2)
+	graphics.draw_line(math.cos(ship:angle()) * (100 + fivesqrt3), math.sin(ship:angle()) * (100 + fivesqrt3), math.cos(arrowAlpha + ship:angle()) * arrowDist, math.sin(arrowAlpha + ship:angle()) * arrowDist, 2)
+	-- graphics.draw_line(math.cos(ship:angle()) * (100 + fivesqrt3), math.sin(ship:angle()) * (100 + fivesqrt3), 0, 0, 2)
 	--[[graphics.set_camera(0, 0, 640, 480)
 	graphics.draw_image("Panels/SideLeft", 31, 240, 69.29, 480)
 	graphics.draw_image("Panels/SideRight", 634, 240, 12.69, 480)
 	--]]
 	-- why did you change the dimensions to 640x480? They were fine before
-    graphics.set_camera(-500, -500, 500, 500)
+	graphics.set_camera(-camera.width / 2.0, -camera.height / 2.0, camera.width / 2.0, camera.width / 2.0)
 	graphics.draw_image("Panels/SideLeft", -435, 0, 129, 1000)
     graphics.draw_image("Panels/SideRight", 487, 0, 27, 1000)
 	graphics.end_frame()
@@ -181,13 +180,26 @@ end
 
 
 
-bullet = { x = 0, y = 0, dest = { x = 100, y = 50 }, velocity = 1, beta = 0, theta = 0, size = { x = 0, y = 0 }, turn_rate = 0.01, ammo = 5 }
+bullet = { x = 0, y = 0, dest = { x = 100, y = 50 }, lock = { x = false, y = false }, velocity = 1, beta = 0, theta = 0, size = { x = 0, y = 0 }, turn_rate = 0.01, ammo = 50 }
 bullet.size.x, bullet.size.y = graphics.sprite_dimensions("Weapons/WhiteYellowMissile")
 firebullet = false
 bulletfired = false
 
 function fire_bullet(x, y, angle)
 	if bullet.ammo > 0 then
+--		local shipLocation = physbullet:location()
+--		local shipVelocity = physbullet:speed()
+		physbullet = PhysicsObject(1.0, shipLocation.x, shipLocation.y, shipVelocity.x, shipVelocity.y, ship:angle())
+		physbullet:set_top_speed(50.0)
+		physbullet:set_top_angular_velocity(0.1)
+		physbullet:set_rotational_drag(0.2)
+		physbullet:set_drag(0.0)
+		local newTime = mode_manager.time()
+		local dt = newTime - lastTime
+		lastTime = newTime
+		force = { x = 0, y = 0 }
+--		physbullet:update(dt, force, 0.0)
+		
 		bullet.x = x
 		bullet.y = y
 		bullet.dest.x = 100
@@ -197,29 +209,32 @@ function fire_bullet(x, y, angle)
 		bullet.ammo = bullet.ammo - 1
 		sound.play("RocketLaunchr")
 		-- temp sound file, should be "RocketLaunch" but for some reason, that file gets errors (file included in git for troubleshooting)
-		graphics.draw_sprite("Gaitori/Carrier", 500, 450, carrierSize[1], carrierSize[2], carrierRotation)
-		graphics.draw_sprite("Weapons/WhiteYellowMissile", x, y, bullet.size.x, bullet.size.y, bullet.theta)
+		local bulletLocation = physbullet:location()
+		graphics.draw_sprite("Weapons/WhiteYellowMissile", x, y, bulletLocation.x, bulletLocation.y, physbullet:angle())
 	end
 end
 
 function moving_bullet()
-	bullet.beta = math.atan2(bullet.dest.y - bullet.y, bullet.dest.x - bullet.x)
+	--[[bullet.beta = math.atan2(bullet.dest.y - bullet.y, bullet.dest.x - bullet.x)
 	if bullet.beta >= bullet.turn_rate then --this if chain changes the angle at which the bullet is going, if necessary
-		bullet.theta = bullet.theta - bullet.turn_rate
-	elseif bullet.beta <= bullet.turn_rate then
 		bullet.theta = bullet.theta + bullet.turn_rate
+	elseif bullet.beta <= bullet.turn_rate then
+		bullet.theta = bullet.theta - bullet.turn_rate
 	elseif bullet.beta < bullet.turn_rate then 
 		if bullet.beta > 0 then
-			bullet.theta = bullet.theta - bullet.beta
+			bullet.theta = bullet.theta + bullet.beta
 		end
 	elseif bullet.beta > bullet.turn_rate then
 		if bullet.beta < 0 then
-			bullet.theta = bullet.theta + bullet.beta
+			bullet.theta = bullet.theta - bullet.beta
 		end
 	end
-	bullet.x = bullet.x + math.cos(bullet.theta) * velocity
-	bullet.y = bullet.y + math.sin(bullet.theta) * velocity
-	graphics.draw_sprite("Weapons/WhiteYellowMissile", bullet.x, bullet.y, bullet.size.x, bullet.size.y, bullet.theta)
+	bullet.x = bullet.x + math.cos(bullet.beta) * bullet.velocity
+	bullet.y = bullet.y + math.sin(bullet.beta) * bullet.velocity
+	bullet.theta = bullet.theta % (math.pi * 2)
+	--]] -- this code is the old turning code
+	local bulletLocation = physbullet:location()
+	graphics.draw_sprite("Weapons/WhiteYellowMissile", bulletLocation.x, bulletLocation.y, bullet.size.x, bullet.size.y, physbullet:angle())
 end
 
 function keyup ( k )
@@ -259,8 +274,10 @@ function key ( k )
 		sound.play("WarpOut")
 	elseif k == " " then
 		sound.play("ShotC")
-		-- drawshot = true;
+		drawshot = true;
 	elseif k == "p" then
 		carrierHealth = 0
+	elseif k == "escape" then
+		-- exit()
 	end
 end
